53daa57f7fbe04b696bf30035d6a47cb65c6b83d
Matheus Brandino <m.v.brandino@gmail.com>
2018-08-03 14:33:51 -0300

    inicio do projeto

---
+++ src/main/java/br/com/caelum/dashforum/DashForumApplication.java
@@ -0,0 +1,12 @@
+package br.com.caelum.dashforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashForumApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/caelum/dashforum/DashForumApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.caelum.dashforum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DashForumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
